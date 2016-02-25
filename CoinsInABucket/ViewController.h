0490680af9f544d2c913f29ce4da1bc26a640cea
Coupang <Coupang@coupangui-macbook-pro.local>
2016-02-25 15:12:46 +0900

    Initial Commit

---
+++ CoinsInABucket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoinsInABucket
+//
+//  Created by Coupang on 2/25/16.
+//  Copyright © 2016 Hogwarts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
